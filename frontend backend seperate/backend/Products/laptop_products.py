5205741142be50296369943d383f63d9f11cbfc7
renganath13 <renganath13@gmail.com>
2025-12-11 12:37:14 +0530

    laptop updated

--- frontend backend seperate/backend/Products/laptop_products.py
+++ frontend backend seperate/backend/Products/laptop_products.py
@@ -1,4 +1,55 @@
 laptop_products = [
     
+
+#  LENOVO LAPTOp
+
+{
+  "id": "laptop-lenovo-ideapad3",
+  "name": "Lenovo IdeaPad 3 (15.6\" FHD)",
+  "brand": "Lenovo",
+  "series": "ideapad",
+  "category": "Laptop",
+  "application": "Home / Office Work / Student Use",
+  "description": "A 15.6-inch everyday laptop with 12th Gen Intel Core i5, 16GB LPDDR5 RAM, fast SSD storage, and Windows 11—ideal for productivity and daily tasks.",
+
+  "processor": "Intel Core i5-12450H (Up to 4.40 GHz)",
+  "memory": "16 GB LPDDR5-4800 (Soldered)",
+  "storage": "512 GB M.2 2242 PCIe Gen4 SSD",
+  "display": "15.6\" FHD (1920×1080), TN, 250 nits, 60 Hz",
+  "graphics": "Integrated Intel UHD Graphics",
+  "network": "Wi-Fi 6 (2×2 AX) + Bluetooth 5.1",
+  "operating_system": "Windows 11 Home",
+  "color": "Arctic Grey",
+  "warranty": "1 Year On-site + Accidental Damage Protection",
+
+  "form_factor": "Laptop",
+  "image": "/static/images/lenovo_consumer_ideapad-3_1.jpg",
+  "price": 52990
+},
+
+{
+  "id": "laptop-lenovo-ideapad5",
+  "name": "Lenovo IdeaPad 5 (15.6\" FHD)",
+  "brand": "Lenovo",
+  "series": "ideapad",
+  "category": "Laptop",
+  "application": "Office Work / Students / Everyday Productivity",
+  "description": "A slim and stylish 15.6-inch laptop with 12th Gen Intel Core i5, fast SSD storage, strong battery life, and a bright FHD IPS display—ideal for productivity and multitasking.",
+
+  "processor": "Intel Core i5-1235U (Up to 4.40 GHz, 10-core)",
+  "memory": "16 GB LPDDR4X-4266 (Soldered)",
+  "storage": "512 GB M.2 NVMe SSD",
+  "display": "15.6\" FHD (1920×1080), IPS, 300 nits, Anti-Glare",
+  "graphics": "Integrated Intel Iris Xe Graphics",
+  "network": "Wi-Fi 6 + Bluetooth 5.1",
+  "operating_system": "Windows 11 Home",
+  "color": "Graphite Grey",
+  "warranty": "1 Year Standard Warranty",
+
+  "form_factor": "Laptop",
+  "image": "/static/images/lenovo_consumer_ideapad-3_1.jpg",
+  "price": 62990
+}
+
     
 ]
