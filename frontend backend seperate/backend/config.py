a3020d8b57f00c5e7575522eb990ffd3ec7f664c
padhuten <padhu2491@gmail.com>
2025-12-12 18:06:32 +0530

    chnages folder structure

--- frontend backend seperate/backend/config.py
+++ frontend backend seperate/backend/config.py
@@ -28,7 +28,7 @@ SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "goguldev28@gmail.com")
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "padhu9549@gmail.com")
 
 # Database envs (read-only here)
 DB_HOST = os.getenv("DB_HOST")
